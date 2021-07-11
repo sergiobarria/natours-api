ee6c417ddb7ef1fa640ba3c1a2c0aadd2e75ed94
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-10 23:23:11 -0500

    add signup & login functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,15 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userControllers');
+const { signup, login } = require('../controllers/authControllers');
 
 const router = express.Router();
 
+// Authentication routes
+router.post('/signup', signup);
+router.post('/login', login);
+
+// Application Routes
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
