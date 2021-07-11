e445f6d2644028266a3edd60749d59ac28943958
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-11 00:19:44 -0500

    add protected routes to tours routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('../controllers/tourControllers');
+const { protect } = require('../controllers/authControllers');
 
 const router = express.Router();
 
@@ -16,7 +17,7 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
