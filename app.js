00e2f4e7b2d5aef419c37d207cb275668e122f6c
Sergio Barria <sergio.barria9210@gmail.com>
2021-07-12 00:17:00 -0500

    add jwt via cookie, request limits, security http headers, data sanitization, avoid parameter pollution

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const colors = require('colors'); // eslint-disable-line
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
 
@@ -11,13 +17,47 @@ const userRouter = require('./routes/userRoutes');
 // Start express app
 const app = express();
 
-// Middlewares
+// Global Middlewares
+// Set security HTTP headers
+app.use(helmet());
+
+// Development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
 
-// Mounting Routers
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (Cross site scripting attack)
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Mounting Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
