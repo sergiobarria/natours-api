4b2d01010781854fcdb245bf1df102456a212ebd
Sergio Barria <sbarria.dev@gmail.com>
2022-05-07 22:13:19 -0500

    add error handling to the API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
+/* eslint-disable no-console */
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from '../swagger.json';
 
+import { APIError } from './utils/apiError.js';
+import { errorHandler } from './middleware/error.controller.js';
+
 import tourRoutes from './routes/tour.routes.js';
 import userRoutes from './routes/user.routes.js';
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config();
 
@@ -25,8 +35,10 @@ app.use('/api/v1/users', userRoutes);
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the Express Server!...');
+app.all('*', (req, res, next) => {
+  next(new APIError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(errorHandler);
 
 export default app;
